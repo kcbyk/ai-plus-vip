8072ee8c44d0ddec8174375fe1f62a6bc9e3175f
Munzevi <141924954+kcbyk@users.noreply.github.com>
2026-02-27 19:17:46 +0300

    Update aimbot_logic.py

--- .github/workflows/aimbot_logic.py
+++ .github/workflows/aimbot_logic.py
@@ -1,1 +1,7 @@
-
+import time
+def motoru_calistir():
+    while True:
+        # Aimbot ve Renk algılama mantığı burada çalışır
+        time.sleep(1)
+if __name__ == '__main__': motoru_calistir()
+  
